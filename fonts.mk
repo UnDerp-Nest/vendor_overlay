8957cf8b36c5e39f3fb115c4fdba2cd3f99461ce
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-05-19 17:39:08 +0700

    vendor: Add 'GoogleSans' font family

---
+++ fonts.mk
@@ -0,0 +1,10 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/overlay/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/overlay/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/overlay/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/overlay/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/overlay/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/overlay/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/overlay/fonts/google-sans.xml
